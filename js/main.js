0b105e96198d83ca6d1eb8d1e6ab0111fb7a5395
imagraphicf-blip <ima.graphic.f@gmail.com>
2026-09-21 01:16:43 +0330

    Delete js directory

--- js/main.js
+++
@@ -1,6 +0,0 @@
-const menuIcon = document.querySelector(".menu-icon");
-const navLinks = document.querySelector(".nav__links");
-
-menuIcon.addEventListener("click", function () {
-    navLinks.classList.toggle("active");
-});
